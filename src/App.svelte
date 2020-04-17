8d83ab69224f9b3f18dd5182fc0c4c55f128b487
Hector Vega <linnk99@gmail.com>
2020-04-17 00:08:31 -0500

    Components class added, creating the About component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import About from "./components/About.svelte"
 	export let name;
   export let lastName;
   let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
@@ -13,11 +14,6 @@
 
 	:global(:root) {
 		--theme-color: purple;
-	}
-
-	p {
-		color: var(--theme-color);
-    font-size: 22px;
 	}
 
 	main {
@@ -47,6 +43,6 @@
 
 <main>
 	<h1>Hello {name} {lastName}!</h1>
-	<p>Automation Tester</p>
+  <About />
   <img src={svelteLogo} alt="Svelte Logo">
 </main>
---
+++ src/components/About.svelte
@@ -0,0 +1,15 @@
+<script>
+  let someText = `Automation Tester @ Platzi`;
+</script>
+
+<style>
+  p {
+    color: var(--theme-color);
+  }
+</style>
+
+<div class="About">
+  <p>
+    {someText}
+  </p>
+</div>
