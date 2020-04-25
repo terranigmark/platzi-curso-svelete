80f0ad0082dafba75ea144cdaf5f5efcdc0fe252
Hector Vega <linnk99@gmail.com>
2020-04-24 22:39:01 -0500

    Each conditional class added

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import About from "./components/About.svelte"
   import Text from "./components/Text.svelte"
   import Person from "./components/Person.svelte"
+  import Skills from "./components/Skills.svelte"
 
   export let name;
   export let lastName;
@@ -66,5 +67,6 @@
   <Text anotherText="Hola!"/>
   <Text />
   <Person {...data}/>
+  <Skills />
   <img src={svelteLogo} alt="Svelte Logo">
 </main>
---
+++ src/components/Skills.svelte
@@ -0,0 +1,42 @@
+<script>
+    let skills = [
+        {
+            id: 1,
+            name: 'Python'
+        },
+        {
+            id: 2,
+            name: 'Selenium'
+        },
+        {
+            id: 3,
+            name: 'Testing'
+        }
+    ]
+</script>
+
+<style>
+    .Skills {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+    li {
+        display: inline;
+        margin: 0 0 0 0.4em;
+    }
+</style>
+
+<div class="Skills">
+    <h3>Habilidades</h3>
+    <ul>
+        {#each skills as { name }, i}
+            <li>{name}</li>
+        {/each}
+    </ul>
+</div>
